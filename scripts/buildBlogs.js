f74ece9f0f095486d2f5c6f67a3debc9374aed39
Zhenyu <1214327383@qq.com>
2020-08-11 23:22:25 +0800

    build blogs

--- scripts/buildBlogs.js
+++ scripts/buildBlogs.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const { inspect } = require('util');
+const mkdirp = require('mkdirp');
 const path = require('path');
 const chalk = require('chalk');
 const fm = require('front-matter');
@@ -6,48 +8,52 @@ const cryptoMd5 = require('md5');
 
 const CACHE_PATH = path.resolve(__dirname, '../.blogs');
 const MD_CACHE_PATH = path.join(CACHE_PATH, 'md');
-const MANIFEST_CACHE_PATH = path.join(CACHE_PATH, 'manifest.js');
+const MANIFEST_FILE = path.join(CACHE_PATH, 'manifest.js');
+const DTS_FILE = path.join(CACHE_PATH, 'blog.d.ts');
 const BLOG_PATH = path.resolve(__dirname, '../src/blogs');
 
+initBlogs();
+
 let manifest;
-if (fs.statSync(MANIFEST_CACHE_PATH).isFile()) {
-  manifest = require(MANIFEST_CACHE_PATH);
+if (fs.existsSync(MANIFEST_FILE)) {
+  manifest = require(MANIFEST_FILE);
 } else {
   manifest = {
     index: 0,
-    titleMap: {},
+    infos: {},
   };
 }
-const { index, titleMap } = manifest;
+let { index, infos } = manifest;
 
 fs.readdirSync(BLOG_PATH).forEach(file => {
   const pth = path.resolve(BLOG_PATH, file);
   const stat = fs.statSync(pth);
   if (stat.isFile() && /.md$/.test(file)) {
+    const name = file.slice(0, file.lastIndexOf('.'));
     let data = fs.readFileSync(pth, { encoding: 'utf-8' });
     // 提取YAML头部
     const content = fm(data);
 
-    if (!titleMap[file]) {
+    if (!infos[name]) {
       index = index + 1;
-      titleMap[file] = { id: index };
+      infos[name] = { id: index };
     }
     const md5 = cryptoMd5(content.body);
-    if (titleMap[file].md5 !== md5) {
-      fs.writeFileSync(path.join(MD_CACHE_PATH, `${index}`), content.body, {
+    if (infos[name].md5 !== md5) {
+      fs.writeFileSync(path.join(MD_CACHE_PATH, `${index}.md`), content.body, {
         encoding: 'utf-8',
       });
-      titleMap[file].md5 = md5;
+      infos[name].md5 = md5;
     }
 
-    fillFrontMatter(titleMap[file], {
-      title: file,
+    fillFrontMatter(infos[name], {
+      title: name,
       ...content.attributes,
     });
   }
 });
 
-fs.writeFileSync(MANIFEST_CACHE_PATH, stringifyManifest(), {
+fs.writeFileSync(MANIFEST_FILE, stringifyManifest(), {
   encoding: 'utf-8',
 });
 
@@ -63,9 +69,38 @@ function stringifyManifest() {
   return (
     `module.exports = {\n` +
     `index: ${index},\n` +
-    `titleMap: ${JSON.stringify(titleMap)}` +
+    `infos: ${inspect(infos)}` +
     `}`
   );
 }
 
-chalk.green('Build Success!');
+function initBlogs() {
+  const dtsTmp = `
+export interface Blog {
+  id: number;
+  md5: string;
+  title: string;
+  description?: string;
+  date?: string;
+  author?: string;
+  tags?: string[];
+  complexity: 'easy' | 'ordinary' | 'hard';
+}
+export interface ManifestBlog {
+  index: number;
+  infos: {
+    [key: string]: Blog;
+  };
+}
+`;
+  if (!fs.existsSync(MD_CACHE_PATH)) {
+    const made = mkdirp.sync(MD_CACHE_PATH);
+    console.log(chalk.green(`Made directories, starting with ${made}`));
+  }
+  fs.writeFile(DTS_FILE, dtsTmp, { encoding: 'utf-8' }, err => {
+    if (err) throw err;
+    console.log(chalk.green(`${path.basename(DTS_FILE)} has been created!`));
+  });
+}
+
+console.log(chalk.green('Build Success!'));
